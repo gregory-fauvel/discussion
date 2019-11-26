27fff2c31ac0ea42a3d2770c5646e035bf122f06
thomas-saussol <55872252+thomas-saussol@users.noreply.github.com>
2019-11-26 16:06:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="discussion.css">
+	<title>Page index</title>
+</head>
+<body id="bodyindex">
+	<header>
+	<?php
+	session_start();
+    if (isset($_SESSION['login']) && ($_SESSION['login'] == true))
+    {
+    include 'barnavco.php';
+}
+    else
+    {
+        include 'barnav.php';
+    }
+
+    ?>
+	</header>
+	
+	<h1 id="titreindex">Bienvenue dans le site  de Discussion</h1>
+
+</section>
+	
+</body>
+</html>
